5a07d8561bad9a6ee24127864547719ebcb65cc0
junclee <510389264@qq.com>
2015-10-09 10:29:02 +0800

    m

---
+++ test02/test02/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test02
+//
+//  Created by ljc on 15/10/8.
+//  Copyright (c) 2015年 LJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
